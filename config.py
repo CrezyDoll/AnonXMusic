326c5fd5d54d0f167a2621297a31485510968b16
CrezyDoll <126459834+CrezyDoll@users.noreply.github.com>
2023-11-06 19:57:05 +0530

    Update config.py
    Signed-off-by: CrezyDoll <126459834+CrezyDoll@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/Red_Wine_Op",
+    "https://github.com/CrezyDoll/AnonXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
